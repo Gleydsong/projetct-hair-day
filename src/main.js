a4de25587cffa152b7ff752e909fbb769b569375
Gleydsong <ggleydson@icloud.com>
2025-11-07 13:05:58 +0000

    Carregando o HTML e CSS e corrigindo algumas funcoes.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+//CSS Imports
+import './styles/global.css';
+import './styles/form.css';
+import './styles/schedule.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -20,4 +22,25 @@ module.exports = {
     liveReload: true,
 
 },
-}
+plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        favicon: path.resolve(__dirname, 'src', 'assets', 'scissors.svg'),
+       
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+            { from: path.resolve(__dirname, 'src', 'assets'), 
+                to: path.resolve(__dirname, 'dist', 'src', 'assets') },
+        ],
+    }),
+],
+module: {
+    rules:[
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+    ],
+},
+};
