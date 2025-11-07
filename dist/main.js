168bfdb46afd888a1cde3b35c9603473328e06b5
Gleydsong <ggleydson@icloud.com>
2025-11-07 12:44:32 +0000

    Instalando o Webpack

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("// Arquivo principal da aplicação Hair Day\r\n\r\nconsole.log('Hair Day - Sistema de Agendamento');\r\n\r\n// Importações futuras virão aqui\r\n// import './styles/global.css'\r\n// import { scheduleNew } from './modules/schedule-new.js'\r\n// import { scheduleFetch } from './modules/schedule-fetch.js'\r\n\r\n// Inicialização da aplicação\r\ndocument.addEventListener('DOMContentLoaded', () => {\r\n  console.log('Aplicação inicializada com sucesso!');\r\n});\r\n\r\n\n\n//# sourceURL=webpack://hairday/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	
+/******/ })()
+;
