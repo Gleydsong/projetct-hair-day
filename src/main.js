82a55e65b9c01efc6f192c333d703868b5e998f5
Gleydsong <ggleydson@icloud.com>
2025-11-07 13:23:01 +0000

    Adicionado o babel e a Lib Dayjs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 "use strict";
 
+//Libs Imports
+import './libs/dayjs.js';
 
 //CSS Imports
 import './styles/global.css';
 import './styles/form.css';
 import './styles/schedule.css';
+
+
+import dayjs from 'dayjs';
+console.log(dayjs().format('DD/MM/YYYY'));
